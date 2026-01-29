70327fa963e8f1980b90b0642ed1401d4e639351
RoboticsKnight <toluwanimiomoyeni30@gmail.com>
2026-01-29 17:48:04 +0200

    Sorting out libraries (WIP)

--- Facial_Emotion_Detection.py
+++ Facial_Emotion_Detection.py
@@ -1,2 +1,14 @@
-print("Hello World")
-print("Ready to work")
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+import tensorflow as tf
+import os
+tf.keras.utils.image_dataset_from_directory
+
+train_data_dir = 'archive/train'
+validation_data_dir = 'archive/test' 
+
+train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=30, shear_range=0.3, zoom_range=0.3, horizontal_flip=True, fill_mode='nearest')
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
